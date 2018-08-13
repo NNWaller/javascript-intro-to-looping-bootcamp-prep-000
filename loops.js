ef364d258688bf14c4e16dce92b03dc4669a1e31
Natasha Waller <nnwaller@gmail.com>
2018-08-13 17:09:34 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+let i;
+
+function forLoop () {
+ for (i = 0; i < 25; i++) {
+   if (i === 1){
+     console.log ("I am 1 strange loop.");
+   } else { 
+     console.log(`I am ${i} strange loops.`);
+
+   }
+ }
+}
+
+
+let n = 37;
+function whileLoop (n){
+  while (n > 0) {
+   console.log(--n);
+  }
+  if (n === 0){
+    return "done";
+  }
+
+}
